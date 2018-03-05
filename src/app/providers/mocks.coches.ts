efd22bc89db14ec34ad08f042f3d1dbfbd957649
anderuraga <ander.uraga@gmail.com>
2018-03-05 09:54:20 +0100

    mas coches

--- src/app/providers/mocks.coches.ts
+++ src/app/providers/mocks.coches.ts
@@ -1,23 +1,95 @@
 export class MOCKS_COCHES {
 
-    static stock = `[
-        {"id" : 12,
-        "marca" : "Seat",
-        "modelo" : "Panda",
-        "version" : "1.0",
-        "foto" : "https://www.minicar.es/large/Fiat-Panda-%281980%29-RBA-Entrega-29-1%3A43-i33233.jpg",
-        "puertas" : 3,
-        "caballos": 100,
-        "consumo": 5},
-        {"id" : 11,
-        "marca" : "Toyoya",
-        "modelo" : "Panda",
-        "version" : "1.0",
-        "foto" : "https://www.minicar.es/large/Fiat-Panda-%281980%29-RBA-Entrega-29-1%3A43-i33233.jpg",
-        "puertas" : 3,
-        "caballos": 100,
-        "consumo": 5}
-    
-    ]`;
+    static stock = `[{
+		"id": 12,
+		"marca": "Seat",
+		"modelo": "Panda",
+		"version": "1.0",
+		"foto": "https://www.minicar.es/large/Fiat-Panda-%281980%29-RBA-Entrega-29-1%3A43-i33233.jpg",
+		"puertas": 3,
+		"caballos": 100,
+		"consumo": 5
+	},
+	{
+		"id": 13,
+		"marca": "Seat",
+		"modelo": "Ibiza",
+		"version": "1.0",
+		"foto": "https://a.ccdn.es/nuevos/400/seat/ibiza/2017/5ha.jpg",
+		"puertas": 5,
+		"caballos": 95,
+		"consumo": 3
+	},
+	{
+		"id": 14,
+		"marca": "Citroen",
+		"modelo": "Saxo",
+		"version": "2.6",
+		"foto": "https://www.automanijak.com/resources/images/variant/381/saxo_2.jpg",
+		"puertas": 5,
+		"caballos": 68,
+		"consumo": 2.7
+	},
+	{
+		"id": 15,
+		"marca": "Bat-movil",
+		"modelo": "Del Caballero Oscuro",
+		"version": "999.999.999",
+		"foto": "http://static.alfabetajuega.com/abj_public_files/multimedia/imagenes/201508/118896.batman-tumbler.jpg",
+		"puertas": 7,
+		"caballos": 230,
+		"consumo": 5.0
+	},
+	{
+		"id": 555,
+		"marca": "Citroen",
+		"modelo": "Palace",
+		"version": "7.0",
+		"foto": "https://upload.wikimedia.org/wikipedia/commons/f/fd/Citroen_GS_Pallas_1977.jpg",
+		"puertas": 5,
+		"caballos": 340,
+		"consumo": 28
+	},
+	{
+		"id": 20,
+		"marca": "Seat",
+		"modelo": "Leon",
+		"version": "1.2 TSI",
+		"foto": "https://www.coches.com/fotos_historicas/seat/Leon/high_6aad986865df7a9bb8fc5f05398fd423.jpg",
+		"puertas": 5,
+		"caballos": 110,
+		"consumo": 6.1
+	},
+	{
+		"id": 22,
+		"marca": "Renault",
+		"modelo": "Clio",
+		"version": "2.0",
+		"foto": "https://www.renault.es/gama-renault/renault-vehiculos-turismos/gama-clio/clio/boites-fermees/att1441077990b5451eab7bfd355bebef5d/renault-clio-fase2-galeria08_ok.jpg",
+		"puertas": 3,
+		"caballos": 120,
+		"consumo": 7
+	},
+	{
+		"id": 35,
+		"marca": "Bugatti",
+		"modelo": "Chiron",
+		"version": "Cupé",
+		"foto": "https://www.diariomotor.com/imagenes/picscache/1920x1600c/bugatti-chiron-2016-mas-p2_1920x1600c.jpg",
+		"puertas": 3,
+		"caballos": 1500,
+		"consumo": 190
+	},
+	{
+		"id": 38,
+		"marca": "Golf",
+		"modelo": "Gti",
+		"version": " 2.0 TSI",
+		"foto": " http://static.carroya.com/vehiculos/1737056/1737056_1_w.jpg",
+		"puertas": 5,
+		"caballos": 230,
+		"consumo": 6.30
+	}
+]`;
 
 }
